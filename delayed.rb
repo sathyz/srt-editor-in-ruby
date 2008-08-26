e75a825e43d59e8e57c6efbdf6d608c8fedef5ad
SatheeshKumar <sathyz@gmail.com>
2008-08-27 03:43:44 +0530

    use millisec in calc

--- delayed.rb
+++ delayed.rb
@@ -3,68 +3,35 @@
 #
 
 class String
-	# two strings of time instances can be added
-	# TODO : subtracted
 
-        def to_tlist( )
-		# convert string to a list - "12:23:34,567" => ["12","23","34","567"]
+        def to_msec( )
+		# convert string in time format to milli sec format 
+		# "12:23:34,567" => "12*60*60*1000 + 23*60*1000 + 34 *1000 + 567"
                 time_list = self.split(":")
                 sec, msec = time_list.pop().split(",") 
 		time_list.push(sec)
-
-                time_list.unshift("%.2d"%0) while time_list.size<3
-
-                time_list.each_with_index {|x,i| 
-			time_list[i] = "%.2d" % x.to_i if x.size<2
-		}
-                
-                time_list.push("%.3d" % msec.to_i)
 		p time_list if $DEBUG
-		time_list
-        end
+		msecs = msec.to_i
+		time_list.reverse!
+		time_list.each_with_index { |x,i| msecs += x.to_i * 60**i * 1000 }
+		p msecs if $DEBUG
+		msecs.to_s
+	end
+end
 
-        def add(other)
-		# >> "12:23:59,545".add("5,455") 
-		# => "12:24:05,000"
-		(p other; p self) if $DEBUG 
-		other_list = other.to_tlist
-		self_list = self.to_tlist
-		result_list = Array.new(4,0)
-		(0..3).each do |i| 
-			i = 3 - i
-			
-			result_list[i] = ( result_list[i].to_i + self_list[i].to_i + other_list[i].to_i )
-			
-			quo = i==3? 1000:60
-			#result_list[i-1] = (result_list[i].to_i/quo).to_s if (result_list[i].to_i/quo > 0)
-			if (result_list[i].to_i/quo > 0)
-				result_list[i-1] = result_list[i]/quo
-				result_list[i] = result_list[i]%quo
-			end
-			
-			result_list[i] = "%.#{i==3? 3:2}d"%result_list[i]
-			p result_list,i,quo if $DEBUG
-		end
-		"%s:%s:%s,%s"%result_list
-        end
+class Fixnum
 
-        def subt( other )
-                other_list = other.to_tlist
-                self_list = self.to_tlist
-                result_list = Array.new(4,0)
-                (0..3).each do |i|
-                        i = 3 - i
-                        quo = i==3? 1000: 60
-
-                        if self_list[i].to_i < other_list[i].to_i
-                                self_list[i] = (self_list[i].to_i + quo ).to_s
-                                self_list[i-1] = (self_list[i-1].to_i - 1).to_s
-                        end
-                        result_list[i] = ( self_list[i].to_i - other_list[i].to_i ) 
-                end
-                p result_list if $DEBUG
-                "%.2d:%.2d:%.2d,%.3d"%result_list
-        end
+	def to_time()
+		# convert millisec to standard time format
+		# 3600 * 1000 = ,000
+		time = self
+		msec = "%.3d"%(time % 1000)
+		time /= 1000
+		time_list = []
+		3.times { time_list.unshift( "%.2d"% (time%60) ) ; time /= 60 }
+		p time_list if $DEBUG
+		[ time_list.join(':'),msec ].join(',')
+	end
 
 end
 
@@ -83,9 +50,6 @@ def srt_delay(srt_file, delay)
 	puts "Open input file #{srt_file}" if $DEBUG
 	seq = "1"
 	line = ""
-
-	dir = "+" #delay.match("^[+-]")
-	delay.delete(dir)
 
 	while (line = srt_in.gets) do
 		#line = srt_in.readline() while line.chop!()!=seq
@@ -109,11 +73,8 @@ def delay(line, delay)
 	delay = delay.delete(dir)
 	p dir if $DEBUG
 
-	if dir == "-"
-	       delayed_line =  "%s --> %s\r\n" % original_instances.collect { |x| x.subt(delay) }
-	else
-	       delayed_line = "%s --> %s\r\n" % original_instances.collect { |x| x.add(delay) }
-	end
+	delayed_line = "%s --> %s\r\n" % original_instances.collect { |x| eval( x.to_msec + dir + delay.to_msec ).to_time }
+
 	p delayed_line if $DEBUG
 	delayed_line
 end
